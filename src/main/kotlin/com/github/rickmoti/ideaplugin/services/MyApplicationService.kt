2f6e7a42877d0e765245f4727b9e4ccc737f1f69
GitHub Action <action@github.com>
2022-12-14 03:25:11 +0000

    Template cleanup

--- src/main/kotlin/com/github/rickmoti/ideaplugin/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/rickmoti/ideaplugin/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.rickmoti.ideaplugin.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.rickmoti.ideaplugin.MyBundle
 
 class MyApplicationService {
 
